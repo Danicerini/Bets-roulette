1dd837edb9db99d86cb6f2f7ec62edfac01e14a5
Danicerini <danielecerini01@gmail.com>
2025-01-15 15:06:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,8 +14,10 @@ document.getElementById('dioForm').addEventListener('submit', function(e) {
         message = "Dio non esiste!";
     } else if (randomNumber > 100) {
         message = "Dio è esistito!";
-    } else {
-        message = "Incredibile, Dio esiste!!!";
+    } else if (randomNumber == 33 || randomNumber == 3){
+        message = "Incredibile, Dio esiste!!! Ed Hai vinto un bonus di 100$";
+    }else if (randomNumber == 666){
+        message = "Dio non esiste, BRAVO! digitando questo numero hai vinto 1902$";
     }
 
     document.getElementById('result').innerHTML = `
